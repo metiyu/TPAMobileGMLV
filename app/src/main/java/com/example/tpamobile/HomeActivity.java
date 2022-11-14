6c48c3be8d7f8060e4420fc088ccb1ba846d32eb
holyvia <holyviamarcellaaa@gmail.com>
2022-11-14 23:10:54 +0700

    add_navbar

--- app/src/main/java/com/example/tpamobile/HomeActivity.java
+++ app/src/main/java/com/example/tpamobile/HomeActivity.java
@@ -1,14 +1,55 @@
 package com.example.tpamobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.tpamobile.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.homee:
+                    replaceFragment(new HomeFragment());
+
+                    break;
+                case R.id.transactionn:
+                    replaceFragment(new TransactionFragment());
+                    break;
+                case R.id.profilee:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                default:
+
+
+            }
+
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
